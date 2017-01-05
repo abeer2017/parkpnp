d0f4acf8cb3b9d3d650109daf6476050b4bf1b60
Abeer2017 <ashere@strivepool.com>
2017-01-05 11:07:33 +0530

    add booking model and initial setting on home page

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,12 @@
+class BookingsController < ApplicationController
+  def create
+    @user = current_user
+    @booking = @user.booking.create(booking_params)
+    redirect_to parking_spaces_path
+  end
+ 
+  private
+    def booking_params
+      params.require(:booking).permit(:total_cost, :start_time, :end_time)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170104061325) do
+ActiveRecord::Schema.define(version: 20170104133116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.decimal  "total_cost"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
+  end
 
   create_table "parking_spaces", force: :cascade do |t|
     t.string   "area_no"
@@ -22,6 +32,16 @@ ActiveRecord::Schema.define(version: 20170104061325) do
     t.boolean  "instant_booking"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "name"
+    t.text     "address"
+    t.integer  "contact"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,5 +65,6 @@ ActiveRecord::Schema.define(version: 20170104061325) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "bookings", "users"
   add_foreign_key "profiles", "users"
 end
