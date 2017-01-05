cd42c7268d90b73c2c55eaa018c4763b94fb6c9a
Abeer2017 <ashere@strivepool.com>
2017-01-05 16:52:42 +0530

    add callback to calculate total cost according to booking time

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,12 +1,13 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :parking_space
-  before_create :total_cost
-
-  private
-  def total_cost
+  before_create :cal_total_cost
+  
+  def cal_total_cost
+    parking_space = ParkingSpace.where(:id =>  self.parking_space_id).first
+    cost = parking_space.cost_per_hour
     st = self.end_time.strftime("%H").to_i
     et = self.start_time.strftime("%H").to_i
-    self.total_cost = (st - et)
+    self.total_cost = (st - et)*cost
   end
 end
