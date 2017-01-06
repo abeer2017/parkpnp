c6e8713777df7ae41c04dcf97ef9bf8ab269cbcf
Abeer2017 <ashere@strivepool.com>
2017-01-06 18:57:03 +0530

    Add concern for booking model

---
+++ app/models/concerns/bookable.rb
@@ -0,0 +1,17 @@
+module Bookable
+  extend ActiveSupport::Concern
+
+  included do
+    belongs_to :user
+    belongs_to :parking_space
+    before_create :cal_total_cost
+  end
+
+  def cal_total_cost
+    parking_space = ParkingSpace.where(:id =>  self.parking_space_id).first
+    cost = parking_space.cost_per_hour
+    st = self.end_time.strftime("%H").to_i
+    et = self.start_time.strftime("%H").to_i
+    self.total_cost = (st - et)*cost
+  end
+end
