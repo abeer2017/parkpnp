3303c1f68761b94699ec825f8b9335aa7ce92747
Abeer2017 <ashere@strivepool.com>
2017-01-10 15:50:30 +0530

    set request route to user

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,10 @@ Rails.application.routes.draw do
 
   resources :user do
     resources :bookings
+    resources :requests
   end
 
   resources :charges
-
-  resources :requests
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
