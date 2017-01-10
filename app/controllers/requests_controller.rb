622462d8b3319e5ae3cd07bdcb29284cc3255b31
Abeer2017 <ashere@strivepool.com>
2017-01-10 18:59:57 +0530

    request approved by admin

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,19 +1,28 @@
 class RequestsController < ApplicationController
 
-  def new
-    @request = Request.new
+  def index
+    if current_user.role == true #Admin
+      @request = Request.all.where(approve: false)
+      if params[:request_id]
+        request = Request.find(params[:request_id])
+        request.update(:approve => true)
+        flash[:notice]="Request successfully approved"
+      end
+    else
+      @request_hold = current_user.requests.where(approve: false)
+      @request_approve = current_user.requests.where(approve: true)
+    end
   end
 
-  def index
-    @request_hold = current_user.requests.where(approve: false)
-    @request_approve = current_user.requests.where(approve: true)
+  def new
+    @request = Request.new
   end
 
   def create
     @user = current_user
     @request = @user.requests.create(request_params)
     if @request.save
-      redirect_to user_requests_path(@request,@user)
+      redirect_to user_requests_path(@user,@request)
     else
       flash.now[:error] = "Could not booking"
       render new
