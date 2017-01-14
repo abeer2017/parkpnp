914423ee728f185489d944bd1f3be5e7579efec4
Abeer2017 <ashere@strivepool.com>
2017-01-14 14:20:25 +0530

    calulate parking cost for all dates

--- app/models/concerns/bookable.rb
+++ app/models/concerns/bookable.rb
@@ -10,15 +10,23 @@ module Bookable
 
   def cal_total_cost
     parking_space = ParkingSpace.where(:id =>  self.parking_space_id).first
-    cost = parking_space.cost_per_hour
-    et = self.end_time.strftime("%H").to_i
-    st = self.start_time.strftime("%H").to_i
-    self.total_cost = (et - st)*cost
+    cost_calculation(parking_space)
   end
 
-  # def availability
-  #   parking_space = ParkingSpace.where(:id =>  self.parking_space_id).first
-  #   parking_space.available = false
-  # end
-
+  private
+  def cost_calculation(parking_space)
+    cost = parking_space.cost_per_hour
+    start_time = self.start_time #Start Date
+    end_time = self.end_time #End Date
+    day_diff = (end_time - start_time).to_i / (24 * 60 * 60) #difference between dates
+    if day_diff == 0
+      start_time = self.start_time.strftime("%H").to_i # Start Time
+      end_time = self.end_time.strftime("%H").to_i # End Time
+      self.total_cost = (end_time - start_time)*cost
+    else day_diff >= 1
+      start_time = 24 - (self.start_time.strftime("%H").to_i) # Start Time
+      end_time = self.end_time.strftime("%H").to_i # End Time
+      self.total_cost = (((day_diff-1)*24)+start_time+end_time)*cost
+    end
+  end
 end
