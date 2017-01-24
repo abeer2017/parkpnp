77ce3b5bef27a8f3aefd611845056dcec7accb60
Abeer2017 <ashere@strivepool.com>
2017-01-24 18:47:02 +0530

    seed data and apply validation

--- app/models/parking_space.rb
+++ app/models/parking_space.rb
@@ -1,4 +1,6 @@
 class ParkingSpace < ApplicationRecord
+  validates :area_no, :cost_per_hour, presence: true
+
   has_many :bookings
   has_many :requests
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create! :name => 'John Doe', :email => 'admin17@parkpnp.com', :role => true, :password => 'admin1234', :password_confirmation => 'admin1234'
+user = User.create!([{name: 'John Doe', email: 'admin@parkpnp.com', role: true, password: '12345678', password_confirmation: '12345678'},
+      {name: 'Mike', email: 'user@parkpnp.com', role: false, password: '12345678', password_confirmation: '12345678'}])
+parking_space = ParkingSpace.create!([{area_no: "A1", cost_per_hour: "10", available: true},
+                {area_no: "B2", cost_per_hour: "7", available: true},
+                {area_no: "C3", cost_per_hour: "4", available: true}])
